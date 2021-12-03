2c7f7a05e5b0e4c2cfb1d4c79617d314e2248222
Cornelis LK <corneliskette2013@gmail.com>
2021-12-03 11:12:01 +0700

    Source code soal No.2

---
+++ 2_D_71180319.py
@@ -0,0 +1,6 @@
+
+hari = str(input("Hi Kiko, Silahkan Masukkan hari yang ingin Anda telusuri: "))
+for a in hari:
+    print(a, end="")
+    #if a == senin:
+    ##    print("kelas ke-1 Algoritma Graf /nkelas ke-2 Sistem Operasi /n kelas ke-3 PAK", a)
